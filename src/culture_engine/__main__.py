d5b6d18f2c9a96b2376ed146b339485d685e9930
Sam Jackson <samjackson7182@yahoo.co.uk>
2026-07-10 18:07:44 +0100

    Written tmdb_client to connect and retrieve response from tmdb and scaffolded it in __main__.py

--- src/culture_engine/__main__.py
+++ src/culture_engine/__main__.py
@@ -1,1 +1,7 @@
-print("Culture Engine is running")
+# import my tmdb_client module to use the search_film function
+from .tmdb_client import search_film
+
+response = search_film("Shoplifters")  # Call the search_film function with the query "Parasite"
+
+print(response.status_code)  # Print the status code of the response
+print(response.json())  # Print the JSON content of the response
---
+++ src/culture_engine/tmdb_client.py
@@ -0,0 +1,37 @@
+# import the httpx library for making HTTP requests
+import httpx
+# import the os library to access environment variables
+import os
+# import the load_dotenv function from the dotenv library to load environment variables from a .env file
+from dotenv import load_dotenv
+
+# load the environment variables from the .env file
+load_dotenv()
+
+# load the TMDB API bearer token from the environment variables
+TMDB_BEARER_TOKEN = os.getenv("TMDB_BEARER_TOKEN")
+
+# Hard code URL for now but in future will live somewhere else (maybe config)
+TMDB_API_URL = "https://api.themoviedb.org/3"
+
+# define a function to search for a film using the TMDB API
+def search_film(query: str):
+    """
+    Inputs:
+        Query (str): The search query for the film. Usually the title of the film.
+
+    Search TMDB for films matching the supplied query.
+
+    Returns:
+        httpx.Response: Raw HTTP response returned by the TMDB API.
+    
+    """
+    # Build full film specific URL for the TMDB API search endpoint
+    url = f"{TMDB_API_URL}/search/movie"
+
+    # Get response from the TMDB API using the httpx library
+    response = httpx.get(url, 
+                         params={"query": query}, 
+                         headers={"Authorization": f"Bearer {TMDB_BEARER_TOKEN}"})
+    
+    return response
