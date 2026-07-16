1a80e7f9aa38923d550b720c5e25549aa91e696c
Sam Jackson <samjackson7182@yahoo.co.uk>
2026-07-16 18:30:35 +0100

    Implemented first culture_engine specific http exception

--- src/culture_engine/tmdb_client.py
+++ src/culture_engine/tmdb_client.py
@@ -5,6 +5,9 @@ import os
 # import the load_dotenv function from the dotenv library to load environment variables from a .env file
 from dotenv import load_dotenv
 
+# import exceptions module to handle custom exceptions
+from culture_engine.exceptions import TMDBAuthenticationError
+
 # load the environment variables from the .env file
 load_dotenv()
 
@@ -15,7 +18,7 @@ TMDB_BEARER_TOKEN = os.getenv("TMDB_BEARER_TOKEN")
 TMDB_API_URL = "https://api.themoviedb.org/3"
 
 # define a function to search for films using the TMDB API
-def search_films_tmdb(query: str):
+def search_films_tmdb(query: str) -> list[dict]:
     """
     Inputs:
         Query (str): The search query for the film. Usually the title of the film.
@@ -23,7 +26,7 @@ def search_films_tmdb(query: str):
     Search TMDB for films matching the supplied query.
 
     Returns:
-        httpx.Response: Raw HTTP response returned by the TMDB API.
+        list[dict]: A list of dictionaries representing the search results from the TMDB API.
     
     """
     # Build full film specific URL for the TMDB API search endpoint
@@ -33,6 +36,10 @@ def search_films_tmdb(query: str):
     response = httpx.get(url, 
                          params={"query": query}, 
                          headers={"Authorization": f"Bearer {TMDB_BEARER_TOKEN}"})
+    
+    if response.status_code == 401:
+        raise TMDBAuthenticationError("Authentication failed. Please check your TMDB API bearer token.")
+    
     # check response status code and raise an exception if the request was not successful
     response.raise_for_status()
     # output formatted JSON response
