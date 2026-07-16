945b68fc7ed7c245f363b6589f5d8ee153f133f4
Sam Jackson <samjackson7182@yahoo.co.uk>
2026-07-16 18:27:30 +0100

    Written my first exception class

---
+++ src/culture_engine/exceptions.py
@@ -0,0 +1,10 @@
+"""
+Module to define custom exceptions for the culture_engine package.
+"""
+
+class TMDBAuthenticationError(Exception):
+    """
+    Exception raised when there is an authentication error with the TMDB API.
+    """
+    pass
+
